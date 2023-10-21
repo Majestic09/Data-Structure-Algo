f040c66f82250a73c3427f9c31649c57343f8edc
Manish <iammanish2109@gmail.com>
2023-10-21 10:57:44 +0530

    min max 2

--- minmax.js
+++ minmax.js
@@ -3,17 +3,27 @@
 const arr = [54, 7, -4, 8, 21, 2, 0, -2];
 
 function findMinMax(arr) {
-    let max = -Infinity;
-    let min = Infinity;
-    for (let ele of arr) {
-        if (max < ele) {
-            max = ele;
-        }
-        if (min > ele) {
-            min = ele;
-        }
+  let max = -Infinity;
+  let min = Infinity;
+  for (let ele of arr) {
+    if (max < ele) {
+      max = ele;
     }
-    console.log(max+"\n"+min)
+    if (min > ele) {
+      min = ele;
+    }
+  }
+  console.log(max + "\n" + min);
 }
-findMinMax(arr)
+findMinMax(arr);
 
+function checkMinMax(arr) {
+  //sorting array in ascending order
+  arr.sort((a, b) => {
+    return a - b;
+  });
+  let max = arr[arr.length - 1];
+  let min = arr[0];
+  console.log(max + "\n" + min);
+}
+checkMinMax(arr);
